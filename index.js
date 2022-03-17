63bd4626489b0e2438e00520626b2de96b73d799
JiggyboyK <korede305@gmail.com>
2022-03-17 10:48:42 +0100

    my algorithm chek

---
+++ index.js
@@ -0,0 +1,6 @@
+let sentence = 'Wizkid o2 arena show was lit'
+function sentenceReader(sentence){
+    console.log(`The length of the sentence is ${sentence.length}`);
+    console.log(`The number of the word in the sentence is ${sentence.split(' ').length}`)
+}
+sentenceReader(sentence)
